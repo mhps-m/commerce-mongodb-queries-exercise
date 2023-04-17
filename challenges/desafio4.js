be772f2f0d70c7e85b6e6428adaac5f0951eaaec
Miguel Soares <miguelanjohps@gmail.com>
2023-04-17 16:25:40 -0300

    feat: :sparkles: Conclui desafio 5

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,2 @@
-db.produtos.find({ vendidos: { $gt: 50, $lt: 100 } }, { _id: false, nome: true, vendidos: true }).sort({ vendidos: 1 });
+db.produtos.find({ vendidos: { $gt: 50, $lt: 100 } }, { _id: false, nome: true, vendidos: true })
+  .sort({ vendidos: 1 });
---
+++ challenges/desafio5.js
@@ -0,0 +1,3 @@
+db.produtos.find({ 
+  $or: [{ curtidas: 36 }, { vendidos: 85 }], 
+}, { _id: false, nome: true, curtidas: true, vendidos: true });
